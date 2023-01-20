4d467bb0974690e8900ef4e78495dd2c0ba4dd28
StasSytnykov <stas82kak05@gmail.com>
2023-01-20 12:15:12 +0200

    fixed bug in port

--- server/src/index.ts
+++ server/src/index.ts
@@ -41,11 +41,7 @@ async function startApolloServer() {
   await new Promise<void>((resolve) =>
     httpServer.listen({ port: 4000 }, resolve)
   );
-  console.log(
-    `🚀 Server ready at http://localhost:4000{
-      server.graphqlPath
-    }`
-  );
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
 startApolloServer();
