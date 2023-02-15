6a447cd3f26a4395293e717038905c2e9beea58f
StasSytnykov <stas82kak05@gmail.com>
2023-02-15 15:07:29 +0200

    try to fix deploy on backedn

--- server/src/index.ts
+++ server/src/index.ts
@@ -39,10 +39,10 @@ async function startApolloServer() {
   });
 
   await new Promise<void>((resolve) =>
-    httpServer.listen({ port: process.env.PORT || 80 }, resolve)
+    httpServer.listen({ port: process.env.PORT || 4000 }, resolve)
   );
   console.log(
-    `🚀 Server ready at http://localhost:${process.env.PORT || 80}${
+    `🚀 Server ready at http://localhost:${process.env.PORT || 4000}${
       server.graphqlPath
     }`
   );
